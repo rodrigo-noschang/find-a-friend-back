7b0d8ba1d3e8964d16aa54d29a0023e9bdb9dcf8
Rodrigo <rodrigo.noschang1@gmail.com>
2023-07-05 15:19:20 -0300

    First version of the database models

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,13 +4,15 @@ import { z } from 'zod';
 const environmentVariablesSchema = z.object({
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
     PORT: z.coerce.number(),
-    JWT_SECRET: z.string()
+    JWT_SECRET: z.string(),
+    DATABASE_URL: z.string()
 });
 
-const { NODE_ENV, PORT, JWT_SECRET } = environmentVariablesSchema.parse(process.env);
+const { NODE_ENV, PORT, JWT_SECRET, DATABASE_URL } = environmentVariablesSchema.parse(process.env);
 
 export const env = {
     NODE_ENV,
     PORT,
-    JWT_SECRET
+    JWT_SECRET,
+    DATABASE_URL
 }
